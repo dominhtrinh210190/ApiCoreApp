7a86ce4d84709bc6e3563096043d5173d2497ea2
do minh trinh <dominhtrinh210190>
2021-12-30 16:40:39 +0700

    add

--- WebApi/WebApi/Startup.cs
+++ WebApi/WebApi/Startup.cs
@@ -30,7 +30,7 @@ namespace WebApi
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1 main" });
             });
         }
 
