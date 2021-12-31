c9d5a498813955c800b95ed557bc3c3757fa0cda
do minh trinh <dominhtrinh210190>
2021-12-31 14:04:58 +0700

    ss

---
+++ WebApi/WebApi/Models/HangHoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class HangHoa
+    {
+        public string ID { get; set; }
+        public string TenSanPham { get; set; }
+        public double DonGia { get; set; }
+    }
+}
