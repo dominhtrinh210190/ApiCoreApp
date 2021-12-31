08d1a01b25003f1c56b7c4805de83dd18b4ed8ac
do minh trinh <dominhtrinh210190>
2021-12-31 10:48:25 +0700

    add dll

--- WebApi/WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/WebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace WebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -25,6 +25,19 @@ namespace WebApi.Controllers
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("Get2")]
+        public IEnumerable<WeatherForecast> Get2()
         {
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
